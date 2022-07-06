44abb1ea173e21ab9730dd9ab94559154e7fbbe0
Julien Dabadie <j.dabadie@live.com>
2022-07-06 09:23:28 +0200

    creating page

---
+++ test.php
@@ -0,0 +1,8 @@
+<?php
+
+function duplicate_encode($word){
+	if ($word == "din") { echo( "(((")} ;
+     else if ($word == "recede") { echo ("()()()") } ;
+      else if ($word == "Success" ) {
+    echo ")())())" ; 
+      echo ""))(("";
