a7afc67c74eb51d46814db1dbc75e86ab0813866
JulienDabs <j.dabadie@live.com>
2022-07-06 10:30:07 +0200

    delete a php file

--- test.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-function duplicate_encode($word){
-	if ($word == "din") { echo( "(((")} ;
-     else if ($word == "recede") { echo ("()()()") } ;
-      else if ($word == "Success" ) {
-    echo ")())())" ; 
-      echo ""))(("";
